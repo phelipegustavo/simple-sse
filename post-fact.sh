072466f8759503dc33d5295d55f1b8fa97aee654
Phelipe Gustavo <phelipediv@gmail.com>
2022-06-23 11:33:37 -0300

    add post fact

---
+++ post-fact.sh
@@ -0,0 +1,4 @@
+curl -X POST \
+ -H "Content-Type: application/json" \
+ -d '{"info": "Shark teeth are constantly replaced throughout life.", "source": "https://en.wikipedia.org/wiki/Shark"}'\
+ -s http://localhost:5555/fact
